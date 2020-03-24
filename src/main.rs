ddbccb0e790dfc4c83eeb3cda96ffa8de8b2030c
Sanjay K <sanjaykdragon@gmail.com>
2020-03-24 16:43:42 -0400

    code formatted

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use serde::Deserialize;
-use ureq;
 use std::io;
+use ureq;
 
 fn read_line() -> Option<String> {
     let mut input = String::new();
     let read_line_result = io::stdin().read_line(&mut input);
-    if read_line_result.is_ok() { Some(input) } else { None }
+    if read_line_result.is_ok() {
+        Some(input)
+    } else {
+        None
+    }
 }
 
 fn main() {
@@ -19,27 +23,24 @@ fn main() {
     let agent = ureq::Agent::new()
         .set("Cookie", &format!("token={}", token))
         .build();
-    let response = agent
-        .get(&api_url)
-        .call()
-        .into_json()
-        .unwrap();
+    let response = agent.get(&api_url).call().into_json().unwrap();
     //0 is hardcoded
     let questions_vec = response["medias"][0]["questions"].clone();
     let questions: Vec<QuestionsData> = serde_json::from_value(questions_vec).unwrap();
     for question in questions {
         //0 is hardcoded
         let question_body = &question.body[0];
-        if question_body.text.len() == 0 { //if there is no normal text, read the html text
+        if question_body.text.len() == 0 {
+            //if there is no normal text, read the html text
             println!("question html: {}", question_body.html);
-        } else { //print the text
+        } else {
+            //print the text
             println!("question text: {}", question_body.text);
         }
     }
 
     println!("hit enter to exit");
-    let mut empty_string = String::new();
-    io::stdin().read_line(&mut empty_string).expect("unable to read line");
+    read_line().unwrap();
 }
 
 #[derive(Debug, Deserialize)]
